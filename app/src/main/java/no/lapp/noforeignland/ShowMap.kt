2fcdf51f66e7a2680ef93dbd3c458d0401a340ef
Lazboy87 <lassehovden@gmail.com>
2020-03-17 19:35:56 +0100

    added new activites

---
+++ app/src/main/java/no/lapp/noforeignland/ShowMap.kt
@@ -0,0 +1,12 @@
+package no.lapp.noforeignland
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowMap : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.show_map)
+    }
+}
